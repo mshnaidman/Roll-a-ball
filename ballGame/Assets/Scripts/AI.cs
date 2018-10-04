55f5c94a8f572f3fe175db207a5ba3bd51b201a3
Matthew Shnaidman <mshnaidman@Matthews-MacBook-Pro.local>
2018-10-04 15:01:54 -0400

    Project Upload

---
+++ ballGame/Assets/Scripts/AI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AI : MonoBehaviour {
+
+    Vector3 pointA = new Vector3(-2f, 0, 7.5f);
+    Vector3 pointB = new Vector3(0, 0, -7.5f);
+
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * .4f, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
+}
---
+++ ballGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    public AudioSource audioSource;
+    public AudioClip lose;
+    public AudioClip collect;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            audioSource.PlayOneShot(collect);
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy")){
+            audioSource.PlayOneShot(lose);
+            collision.gameObject.SetActive(false);
+            speed = 0;
+            winText.text = "You Lose";
+        }
+    }
+
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
